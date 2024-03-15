2bfe73d40b6433d18dcfa096297a08a6fc922d30
Mumtaz Ahmad <hasaanahmad059@gmail.com>
2024-03-15 11:37:39 +0000

    pagination not added

--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -1,7 +1,6 @@
 import List from "@/Components/List";
 import { useQuery, gql } from "@apollo/client";
-import { Loading, Button } from "@web3uikit/core";
-import { useEffect, useState } from "react";
+import { Loading } from "@web3uikit/core";
 
 const GET_USER_INFO = gql`
   {
@@ -12,24 +11,8 @@ const GET_USER_INFO = gql`
 `;
 
 export default function Marketplace() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const { loading, error, data: initialData } = useQuery(GET_USER_INFO);
-  const itemsPerPage = 3;
-  const [data, setData] = useState([]);
+  const { loading, error, data } = useQuery(GET_USER_INFO);
 
-  useEffect(() => {
-    if (initialData) {
-      setData(initialData.educationVerifieds);
-    }
-  }, [initialData]);
-
-  const nextPage = () => {
-    setCurrentPage(currentPage + 1);
-  };
-
-  const prevPage = () => {
-    setCurrentPage(currentPage - 1);
-  };
   if (loading)
     return (
       <div
@@ -51,20 +34,9 @@ export default function Marketplace() {
     );
   if (error) return <p>Failed to fetch data. Please contract the deployer</p>;
 
-  if (!Array.isArray(data)) return <p>Data is not an array.</p>;
-
-  // const propData = data.educationVerifieds;
-  // const pagnationData = propData.map((item) => {
-  //   return item;
-  // });
-
-  const totalPages = Math.ceil(data.length / itemsPerPage);
-  const lastItemIndex = currentPage * itemsPerPage;
-  const firstItemIndex = lastItemIndex - itemsPerPage;
-  const currentItems = data.slice(firstItemIndex, lastItemIndex);
-
+  const propData = data.educationVerifieds;
   console.log("current items");
-  console.log(currentItems);
+  console.log(propData);
 
   return (
     <div
@@ -74,40 +46,9 @@ export default function Marketplace() {
         List of registered talents with verified education
       </h3>
       <div className="market--place">
-        {/* {propData.map((item, index) => (
-          <List key={index} data={[item]} />
-        ))} */}
-        {currentItems.map((item, index) => (
+        {propData.map((item, index) => (
           <List key={index} data={[item]} />
         ))}
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          gap: "20px",
-          paddingTop: "20px",
-        }}
-      >
-        <Button
-          text="Prev"
-          theme="secondary"
-          onClick={prevPage}
-          disabled={currentPage === 1}
-        />
-        <Button
-          text="Next"
-          theme="secondary"
-          onClick={nextPage}
-          disabled={currentPage === totalPages}
-        />
-        {/* <button onClick={prevPage} disabled={currentPage === 1}>
-          Prev
-        </button>
-        <button onClick={nextPage} disabled={currentPage === totalPages}>
-          Next
-        </button> */}
       </div>
     </div>
   );
